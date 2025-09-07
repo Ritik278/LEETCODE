da73989fa3adc92bb9caaaec6b414fd7452ae8aa
Ritik singh <127231475+Ritik278@users.noreply.github.com>
2025-09-07 09:44:02 +0530

    Time: 2 ms (83.88%) | Memory: 46.2 MB (13.65%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums[0]==0 && nums.length ==1){
-            return true;
-        }
+        // if(nums[0]==0 && nums.length ==1){
+        //     return true;
+        // }
 
         int maxpower =0;
 
