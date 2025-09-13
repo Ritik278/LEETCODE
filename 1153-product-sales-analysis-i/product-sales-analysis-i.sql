a5ae6efee8fc653a4fe142ff912fe6d38d1dcc40
Ritik singh <127231475+Ritik278@users.noreply.github.com>
2025-09-13 14:22:08 +0530

    Time: 1252 ms (58.14%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,1 +1,1 @@
-SELECT p.product_name, s.year, s.price from Sales s join Product p on s.product_id = p.product_id where s.sale_id;
+SELECT p.product_name, s.year, s.price from Sales s join Product p on s.product_id = p.product_id;
